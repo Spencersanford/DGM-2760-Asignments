e6874ce0452f96565519e9af4ddeae961483d784
Spencersandford <10735471@my.uvu.edu>
2021-04-16 16:31:53 -0600

    Done

---
+++ Unit08/main.js
@@ -0,0 +1,33 @@
+const question = {
+  stem: "Who is buried in Grant's Tomb?",
+  option1: "Tom",
+  option2: 'Grant',
+  option3: 'Reggie',
+  option4: 'Roscoe',
+  corret: 2,
+  display: () => {
+    document.querySelector('#stem').textContent = question.stem
+    document.querySelector('#answer1').textContent = question.option1
+    document.querySelector('#answer2').textContent = question.option2
+    document.querySelector('#answer3').textContent = question.option3
+    document.querySelector('#answer4').textContent = question.option4
+    // display the question options here
+  },
+  check: (userChoice) => {
+    if (userChoice === question.corret) {
+      document.querySelector('#feedback').textContent = "You are correct"
+    }
+
+    else {
+      document.querySelector('#feedback').textContent = "Wrong"
+    }
+
+  }
+}
+
+document.querySelector('#answer1').addEventListener('click', () => question.check(1))
+document.querySelector('#answer2').addEventListener('click', () => question.check(2))
+document.querySelector('#answer3').addEventListener('click', () => question.check(3))
+document.querySelector('#answer4').addEventListener('click', () => question.check(4))
+
+question.display()
