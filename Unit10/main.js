921202c08adb98038e1b921ac27d396b77882915
Spencersandford <10735471@my.uvu.edu>
2021-04-27 18:34:22 -0600

    Finishing up

--- Unit10/main.js
+++ Unit10/main.js
@@ -1,6 +1,6 @@
 async function getHotelData(){
   try {
-    const respone = await fetch('../hotel.json')
+    const respone = await fetch('hotel.json')
     return await respone.json()
 
   } catch(error) {
@@ -8,4 +8,24 @@ async function getHotelData(){
   }
 }
 
-console.log(getHotelData)
+let hotelData = {}
+getHotelData().then(data => hotelData = data)
+
+document.querySelector("#marriot").addEventListener('click', hotelInfo)
+
+
+var buttons = document.querySelectorAll("button")
+buttons.forEach(element => addEventListener('click', hotelInfo))
+
+function hotelInfo(event){
+    let hotelChoice = hotelData.hotels.find(hotel => {
+      return event.target.id === hotel.name.toLowerCase()
+    })
+
+    document.querySelector("#hotelName").textContent = `${hotelChoice.name} Hotel`
+    document.querySelector("#Adress").textContent = `${hotelChoice.adress}`
+    document.querySelector("#rooms").textContent = `${hotelChoice.rooms} rooms available `
+    document.querySelector("#gym").textContent = `${hotelChoice.rooms} rooms available `
+    document.querySelector("#RoomType").textContent = `${hotelChoice.roomType}`
+    document.querySelector("#picture").setAttribute("src", hotelChoice.picture)
+}
